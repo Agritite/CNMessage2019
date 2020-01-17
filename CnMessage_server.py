e03540a3f09c1f47439ba94ed71a3edbfde77e1a
isaswa <20658000+isaswa@users.noreply.github.com>
2020-01-18 01:42:03 +0800

    beta ver.3

--- CnMessage_server.py
+++ CnMessage_server.py
@@ -1,12 +1,11 @@
 import socket 
 import select 
 import sys 
-from Queue import Queue
 from _thread import *
-  
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-  
+
 # checks whether sufficient arguments have been provided 
 if len(sys.argv) != 3: 
     print("Correct usage: script, IP address, port number")
@@ -24,135 +23,168 @@ server.listen(10)
 # userlist[username]=hash(pw)
 user_list = {}
 # connected_client[username] = socket
+# online user
 connected_client = {}
-#list of sockets
-list_of_clients = [] 
-#[dest, msg]
-online_msg = Queue()
-#[dest, msg]
-offline_msg = Queue()
+#[from, to, msg]
+online_msg = []
+#[from, to, msg]
+offline_msg = []
+#[from, to, filename, data]
+file_msg = []
+#historical 
 
 
-def clientthread(conn, addr, qOnline, qOffline): 
+
+def clientthread(conn, addr, online_msg, offline_msg, file_msg): 
   
     # sends a message to the client whose user object is conn 
     # conn.send("Welcome to this chatroom!") 
     login_flag = False 
     current_username = ''
-    msg_log = []
 
     while True: 
         try: 
             # reading: non-blocking, timeout = 1s
             readable, _, _ = select.select([conn], [], [], 1)
             if conn in readable :
-                status = int(conn.recv(1))
+                status = int(conn.recvall(1).decode())
             else : # nothing to read
                 status = -1 
 
             # login
-            if status = 0 :
-                username_size = int(conn.recv(4))
-                username = conn.recv(username_size)
-                password_size = int(conn.recv(4))
-                password = conn.recv(password_size)
+            if status == 0 :
+                username_size = int.from_bytes(conn.recvall(4), byteorder='little')
+                username = conn.recvall(username_size).decode()
+                password_size = int(conn.recvall(4).decode())
+                password = conn.recvall(password_size).decode()
 
                 #login success
                 if username in user_list and user_list[username] == hash(password) :
-                    conn.send('1')
+                    conn.send(bytes([1]))
                     login_flag = True
                     current_username = username 
                     connected_client[current_username] = conn
                     print('login succuss: ' + current_username)
                 else :
-                    conn.send('0')
+                    conn.send(bytes([0]))
                     print('login failed: ' + username)
-
-                # check offline message
-                if login_flag = True :
-                    # TODO: check offline message
+                    conn.close()
+                    break
 
             # register 
-            else if status = 1 :
-                username_size = int(conn.recv(4))
-                username = conn.recv(username_size)
-                password_size = int(conn.recv(4))
-                password = conn.recv(password_size)
+            elif status == 1 :
+                username_size = int(conn.recvall(4).decode())
+                username = conn.recvall(username_size).decode()
+                password_size = int(conn.recvall(4).decode())
+                password = conn.recvall(password_size).decode()
                 # register failed
                 if username in user_list :
-                    conn.send('0')
+                    conn.send(bytes([0]))
                     print('username used.')
                 else :
-                    conn.send('1')
+                    conn.send(bytes([1]))
                     login_flag = True
                     current_username = username
                     print('registered: ' + username)
-
+                    conn.close()
+                    break
+            
             # query history
-            else if status = 2 :
+            # elif status == 2 :
                 # TODO
 
             # send msg
-            else if status = 3 :
-                username_size = int(conn.recv(4))
-                dest = conn.recv(username_size)
-                msg_size = int(conn.recv(4))
-                message = conn.recv(msg_size)
+            elif status == 3 :
+                username_size = conn.recvall(4)
+                username_size = int.from_bytes(username_size, byteorder='little')
+                dest = conn.recvall(username_size).decode()
 
-                if message : 
-                    # prints the message and address of the user who just sent the message on the server terminal
-                    print(current_username + ' to ' + dest " : " + message)
-                    # send message
-                    dest_sock = connected_client[dest]
-                    send_msg(message, dest_sock) 
-                # connection broken
-                else: 
-                    """message may have no content if the connection 
-                    is broken, in this case we remove the connection"""
-                    remove(conn)
+                msg_size = conn.recvall(4)
+                msg_size = int.from_bytes(msg_size, byteorder='little')
+                message = conn.recvall(msg_size).decode()
+
+                # prints the message of the user who just sent the message on the server terminal
+                print(current_username + ' to ' + dest + ' : ' + message)
+                # put msg in list
+                if dest not in user_list :
+                    conn.send(bytes([0]))
+                elif dest in connected_client : # online
+                    OnlineMsg.append([current_username, dest, message])
+                else : #offline
+                    OfflineMsg.append([current_username, dest, message])
 
             # file transfer
-            else if status = 4 :
-                # TODO
+            elif status == 4 :
+                username_size = conn.recvall(4)
+                username_size = int.from_bytes(username_size, byteorder='little')
+                dest = conn.recvall(username_size).decode()
 
-            # check 
+                fname_size = conn.recvall(4)
+                fname_size = int.from_bytes(msg_size, byteorder='little')
+                filename = conn.recvall(fname_size)
+
+                file_size = conn.recvall(4)
+                file_size = int.from_bytes(file_size, byteorder='little')
+                filedata = conn.recvall(file_size)
+
+                # prints the message of the user who just sent the file on the server terminal
+                print(current_username + 'send file to ' + dest)
+                # put msg in list
+                if dest not in connected_client : #offline
+                    conn.send(bytes([0]))
+                else : #online
+                    file_msg.append([current_username, dest, filename, filedata])
+
+            #log out
+            elif status == 5 :
+                conn.close()
+                del connected_client[current_username]
+                break
 
 
-            
+            # TODO : disconnect ?
+
+
+            # check online msg
+            for msg in online_msg :
+                if msg[1] == current_username :
+                    conn.send(bytes([13]))
+
+                    conn.send(bytes(len(msg[0])))
+                    conn.send(msg[0].encode())
+
+                    conn.send(bytes(len(msg[2])))
+                    conn.send(msg[2].encode())
+                    
+                    OnlineMsg.remove(msg)
+
+            # check online file
+            for msg in file_msg :
+                if msg[1] == current_username :
+                        conn.send(bytes([14]))
+
+                        conn.send(bytes(len(msg[0])))
+                        conn.send(msg[0].encode())
+
+                        conn.send(bytes(len(msg[2])))
+                        conn.send(msg[2].encode())
+
+                        file_msg.remove(msg)
 
         except: 
+            print('exception occured.')
             continue
-  
-# send msg to destination client 
-def send_msg(message, client): 
-    if client in list_of_clients: 
-        try: 
-            #TODO : client format
-            client.send(message)
-        except: 
-            # if the link is broken, we remove the client 
-            client.close()
-            remove(client) 
-            offline_msg
-
-# remove socket from list_of_clients
-def remove(connection): 
-    if connection in list_of_clients: 
-        list_of_clients.remove(connection) 
-        #TODO: remove from connected_client
+        
   
 while True: 
-  
+
     conn, addr = server.accept() 
-  
-    # Maintains a list of clients for ease of sending a message to available people in the chatroom
-    list_of_clients.append(conn) 
-  
+
     # prints the address of the user that just connected 
     print(addr[0] + " connected")
   
     # creates and individual thread for every user
-    start_new_thread(clientthread,(conn,addr,online_msg,offline_msg))     
+    start_new_thread(clientthread, (conn, addr, online_msg, offline_msg))     
   
 conn.close() 
 server.close() 
